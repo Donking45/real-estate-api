cf0dc815e8ea7d965eecb08982c205128006d937
Donking45 <kingsleyokon610@gmail.com>
2025-06-18 03:58:45 +0100

    updates

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, forgotPassword, resetPassword } = require('../controllers/authController');
+const { register, login, verifyEmail, forgotPassword, verifyOTP, resetPassword } = require('../controllers/authController');
 const { authorization, validateRegister } = require('../middleware/authMiddleware');
 
 
 router.post('/auth/register', validateRegister, register);
 router.post('/auth/login', login);
-router.post('/forgot-password', forgotPassword);
-router.patch('/reset-password', authorization, resetPassword);
+router.post('/auth/verify-email', verifyEmail)
+router.post('/auth/forgot-password', forgotPassword);
+router.post('/auth/verify-otp', verifyOTP)
+router.patch('/auth/reset-password', authorization, resetPassword);
 
 
 module.exports = router;
